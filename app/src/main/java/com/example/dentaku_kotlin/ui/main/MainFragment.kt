a3bac596abe6eeb67f0b91412c05712235f988c8
ha-akimoto <nagiha0000@yahoo.co.jp>
2020-04-01 14:45:53 +0900

    変数名修正

--- app/src/main/java/com/example/dentaku_kotlin/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/dentaku_kotlin/ui/main/MainFragment.kt
@@ -5,16 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
 import com.example.dentaku_kotlin.R
 import com.example.dentaku_kotlin.common.Calculation
 import com.example.dentaku_kotlin.common.Constants
 import com.example.dentaku_kotlin.common.OperatorFlg
-import kotlinx.android.synthetic.main.main_fragment.*
 import com.example.dentaku_kotlin.databinding.*
 
 class MainFragment : Fragment() {
@@ -25,7 +21,7 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
     private lateinit var binding: MainFragmentBinding
-    private val buf :StringBuilder = java.lang.StringBuilder()
+    private val sb :StringBuilder = java.lang.StringBuilder()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
@@ -40,98 +36,98 @@ class MainFragment : Fragment() {
 
         // リスナー設定
         this.binding.button0.setOnClickListener{
-            this.buf.append("0")
-            this.viewModel.text.value = this.buf.toString()
+            this.sb.append("0")
+            this.viewModel.text.value = this.sb.toString()
             flgSet()
         }
         this.binding.button1.setOnClickListener{
-            this.buf.append("1")
-            this.viewModel.text.value = this.buf.toString()
+            this.sb.append("1")
+            this.viewModel.text.value = this.sb.toString()
             flgSet()
         }
         this.binding.button2.setOnClickListener{
-            this.buf.append("2")
-            this.viewModel.text.value = this.buf.toString()
+            this.sb.append("2")
+            this.viewModel.text.value = this.sb.toString()
             flgSet()
         }
         this.binding.button3.setOnClickListener{
-            this.buf.append("3")
-            this.viewModel.text.value = this.buf.toString()
+            this.sb.append("3")
+            this.viewModel.text.value = this.sb.toString()
             flgSet()
         }
         this.binding.button4.setOnClickListener{
-            this.buf.append("4")
-            this.viewModel.text.value = this.buf.toString()
+            this.sb.append("4")
+            this.viewModel.text.value = this.sb.toString()
             flgSet()
         }
         this.binding.button5.setOnClickListener{
-            this.buf.append("5")
-            this.viewModel.text.value = this.buf.toString()
+            this.sb.append("5")
+            this.viewModel.text.value = this.sb.toString()
             flgSet()
         }
         this.binding.button6.setOnClickListener{
-            this.buf.append("6")
-            this.viewModel.text.value = this.buf.toString()
+            this.sb.append("6")
+            this.viewModel.text.value = this.sb.toString()
             flgSet()
         }
         this.binding.button7.setOnClickListener{
-            this.buf.append("7")
-            this.viewModel.text.value = this.buf.toString()
+            this.sb.append("7")
+            this.viewModel.text.value = this.sb.toString()
             flgSet()
         }
         this.binding.button8.setOnClickListener{
-            this.buf.append("8")
-            this.viewModel.text.value = this.buf.toString()
+            this.sb.append("8")
+            this.viewModel.text.value = this.sb.toString()
             flgSet()
         }
         this.binding.button9.setOnClickListener{
-            this.buf.append("9")
-            this.viewModel.text.value = this.buf.toString()
+            this.sb.append("9")
+            this.viewModel.text.value = this.sb.toString()
             flgSet()
         }
         this.binding.buttonPlus.setOnClickListener{
-            this.buf.append(Constants.PLUS)
+            this.sb.append(Constants.PLUS)
             this.viewModel.opeFlg.value = OperatorFlg.PLUS
-            this.viewModel.text.value = this.buf.toString()
+            this.viewModel.text.value = this.sb.toString()
             this.viewModel.dotFlg.value = true
             this.viewModel.opeEnableFlg.value = false
         }
         this.binding.buttonMinus.setOnClickListener{
-            this.buf.append(Constants.MINUS)
+            this.sb.append(Constants.MINUS)
             this.viewModel.opeFlg.value = OperatorFlg.MINUS
-            this.viewModel.text.value = this.buf.toString()
+            this.viewModel.text.value = this.sb.toString()
             this.viewModel.dotFlg.value = true
             this.viewModel.opeEnableFlg.value = false
         }
         this.binding.buttonTimes.setOnClickListener{
-            this.buf.append(Constants.TIMES)
+            this.sb.append(Constants.TIMES)
             this.viewModel.opeFlg.value = OperatorFlg.TIMES
-            this.viewModel.text.value = this.buf.toString()
+            this.viewModel.text.value = this.sb.toString()
             this.viewModel.dotFlg.value = true
             this.viewModel.opeEnableFlg.value = false
         }
         this.binding.buttonDivided.setOnClickListener{
-            this.buf.append(Constants.DIVIDED)
+            this.sb.append(Constants.DIVIDED)
             this.viewModel.opeFlg.value = OperatorFlg.DIVIDED
-            this.viewModel.text.value = this.buf.toString()
+            this.viewModel.text.value = this.sb.toString()
             this.viewModel.dotFlg.value = true
             this.viewModel.opeEnableFlg.value = false
         }
         this.binding.buttonDot.setOnClickListener{
-            this.buf.append(".")
-            this.viewModel.text.value = this.buf.toString()
+            this.sb.append(".")
+            this.viewModel.text.value = this.sb.toString()
             this.viewModel.dotFlg.value = false
         }
         this.binding.buttonEquals.setOnClickListener{
             executeCalculation()
             this.viewModel.opeFlg.value = OperatorFlg.DEFAULT
-            this.buf.clear()
+            this.sb.clear()
             this.viewModel.dotFlg.value = true
             this.viewModel.equalFlg.value = false
             this.viewModel.opeEnableFlg.value = false
         }
         this.binding.buttonClear.setOnClickListener{
-            this.buf.clear()
+            this.sb.clear()
             this.viewModel.text.value = null
             this.viewModel.opeFlg.value = OperatorFlg.DEFAULT
             this.viewModel.dotFlg.value = true
@@ -146,20 +142,20 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         // ViewModelのテキストがNullか空文字じゃない場合
         if(!this.viewModel.text.value.isNullOrEmpty()){
-            this.buf.append(this.viewModel.text.value)
+            this.sb.append(this.viewModel.text.value)
         }
     }
 
     private fun executeCalculation(){
         // それぞれの計算を呼び出す
         if(this.viewModel.opeFlg.value == OperatorFlg.PLUS){
-            this.viewModel.text.value = Calculation.calPlus(this.buf)
+            this.viewModel.text.value = Calculation.calPlus(this.sb)
         } else if (this.viewModel.opeFlg.value == OperatorFlg.MINUS){
-            this.viewModel.text.value = Calculation.calMinus(this.buf)
+            this.viewModel.text.value = Calculation.calMinus(this.sb)
         } else if (this.viewModel.opeFlg.value == OperatorFlg.TIMES){
-            this.viewModel.text.value = Calculation.calTimes(this.buf)
+            this.viewModel.text.value = Calculation.calTimes(this.sb)
         } else if (this.viewModel.opeFlg.value == OperatorFlg.DIVIDED){
-            this.viewModel.text.value = Calculation.calDivided(this.buf)
+            this.viewModel.text.value = Calculation.calDivided(this.sb)
         }
         // 小数点以下が０の場合は削除する
         this.viewModel.text.value = Calculation.zeroCut(this.viewModel.text.value!!)
