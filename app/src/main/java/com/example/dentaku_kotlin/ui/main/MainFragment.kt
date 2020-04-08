f8f1fb685ea1fffd0cb0ff6e858a2ea62a1f3a8e
ha-akimoto <nagiha0000@yahoo.co.jp>
2020-04-08 16:12:43 +0900

    修正：StringBuilderをviewModelで保持するよう変更

--- app/src/main/java/com/example/dentaku_kotlin/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/dentaku_kotlin/ui/main/MainFragment.kt
@@ -21,7 +21,7 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
     private lateinit var binding: MainFragmentBinding
-    private val sb: StringBuilder = java.lang.StringBuilder()
+    //private val sb: StringBuilder = java.lang.StringBuilder()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,98 +40,98 @@ class MainFragment : Fragment() {
 
         // リスナー設定
         this.binding.button0.setOnClickListener {
-            this.sb.append("0")
-            this.viewModel.text.value = this.sb.toString()
+            this.viewModel.sb.value!!.append("0")
+            this.viewModel.sbToText()
             flgSet()
         }
         this.binding.button1.setOnClickListener {
-            this.sb.append("1")
-            this.viewModel.text.value = this.sb.toString()
+            this.viewModel.sb.value!!.append("1")
+            this.viewModel.sbToText()
             flgSet()
         }
         this.binding.button2.setOnClickListener {
-            this.sb.append("2")
-            this.viewModel.text.value = this.sb.toString()
+            this.viewModel.sb.value!!.append("2")
+            this.viewModel.sbToText()
             flgSet()
         }
         this.binding.button3.setOnClickListener {
-            this.sb.append("3")
-            this.viewModel.text.value = this.sb.toString()
+            this.viewModel.sb.value!!.append("3")
+            this.viewModel.sbToText()
             flgSet()
         }
         this.binding.button4.setOnClickListener {
-            this.sb.append("4")
-            this.viewModel.text.value = this.sb.toString()
+            this.viewModel.sb.value!!.append("4")
+            this.viewModel.sbToText()
             flgSet()
         }
         this.binding.button5.setOnClickListener {
-            this.sb.append("5")
-            this.viewModel.text.value = this.sb.toString()
+            this.viewModel.sb.value!!.append("5")
+            this.viewModel.sbToText()
             flgSet()
         }
         this.binding.button6.setOnClickListener {
-            this.sb.append("6")
-            this.viewModel.text.value = this.sb.toString()
+            this.viewModel.sb.value!!.append("6")
+            this.viewModel.sbToText()
             flgSet()
         }
         this.binding.button7.setOnClickListener {
-            this.sb.append("7")
-            this.viewModel.text.value = this.sb.toString()
+            this.viewModel.sb.value!!.append("7")
+            this.viewModel.sbToText()
             flgSet()
         }
         this.binding.button8.setOnClickListener {
-            this.sb.append("8")
-            this.viewModel.text.value = this.sb.toString()
+            this.viewModel.sb.value!!.append("8")
+            this.viewModel.sbToText()
             flgSet()
         }
         this.binding.button9.setOnClickListener {
-            this.sb.append("9")
-            this.viewModel.text.value = this.sb.toString()
+            this.viewModel.sb.value!!.append("9")
+            this.viewModel.sbToText()
             flgSet()
         }
         this.binding.buttonPlus.setOnClickListener {
-            this.sb.append(Constants.PLUS)
+            this.viewModel.sb.value!!.append(Constants.PLUS)
             this.viewModel.opeFlg.value = OperatorFlg.PLUS
-            this.viewModel.text.value = this.sb.toString()
+            this.viewModel.sbToText()
             this.viewModel.dotFlg.value = true
             this.viewModel.opeEnableFlg.value = false
         }
         this.binding.buttonMinus.setOnClickListener {
-            this.sb.append(Constants.MINUS)
+            this.viewModel.sb.value!!.append(Constants.MINUS)
             this.viewModel.opeFlg.value = OperatorFlg.MINUS
-            this.viewModel.text.value = this.sb.toString()
+            this.viewModel.sbToText()
             this.viewModel.dotFlg.value = true
             this.viewModel.opeEnableFlg.value = false
         }
         this.binding.buttonTimes.setOnClickListener {
-            this.sb.append(Constants.TIMES)
+            this.viewModel.sb.value!!.append(Constants.TIMES)
             this.viewModel.opeFlg.value = OperatorFlg.TIMES
-            this.viewModel.text.value = this.sb.toString()
+            this.viewModel.sbToText()
             this.viewModel.dotFlg.value = true
             this.viewModel.opeEnableFlg.value = false
         }
         this.binding.buttonDivided.setOnClickListener {
-            this.sb.append(Constants.DIVIDED)
+            this.viewModel.sb.value!!.append(Constants.DIVIDED)
             this.viewModel.opeFlg.value = OperatorFlg.DIVIDED
-            this.viewModel.text.value = this.sb.toString()
+            this.viewModel.sbToText()
             this.viewModel.dotFlg.value = true
             this.viewModel.opeEnableFlg.value = false
         }
         this.binding.buttonDot.setOnClickListener {
-            this.sb.append(".")
-            this.viewModel.text.value = this.sb.toString()
+            this.viewModel.sb.value!!.append(".")
+            this.viewModel.sbToText()
             this.viewModel.dotFlg.value = false
         }
         this.binding.buttonEquals.setOnClickListener {
             executeCalculation()
             this.viewModel.opeFlg.value = OperatorFlg.DEFAULT
-            this.sb.clear()
+            this.viewModel.sb.value!!.clear()
             this.viewModel.dotFlg.value = true
             this.viewModel.equalFlg.value = false
             this.viewModel.opeEnableFlg.value = false
         }
         this.binding.buttonClear.setOnClickListener {
-            this.sb.clear()
+            this.viewModel.sb.value!!.clear()
             this.viewModel.text.value = null
             this.viewModel.opeFlg.value = OperatorFlg.DEFAULT
             this.viewModel.dotFlg.value = true
@@ -145,25 +145,28 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         // ViewModelのテキストがNullか空文字じゃない場合
+        /*
         if (!this.viewModel.text.value.isNullOrEmpty()) {
-            this.sb.append(this.viewModel.text.value)
+            this.viewModel.sb.value!!.append(this.viewModel.text.value)
         }
+
+         */
     }
 
     private fun executeCalculation() {
         // それぞれの計算を呼び出す
         when (this.viewModel.opeFlg.value) {
             OperatorFlg.PLUS -> {
-                this.viewModel.text.value = Calculation.calPlus(this.sb)
+                this.viewModel.text.value = Calculation.calPlus(this.viewModel.sb.value!!)
             }
             OperatorFlg.MINUS -> {
-                this.viewModel.text.value = Calculation.calMinus(this.sb)
+                this.viewModel.text.value = Calculation.calMinus(this.viewModel.sb.value!!)
             }
             OperatorFlg.TIMES -> {
-                this.viewModel.text.value = Calculation.calTimes(this.sb)
+                this.viewModel.text.value = Calculation.calTimes(this.viewModel.sb.value!!)
             }
             OperatorFlg.DIVIDED -> {
-                this.viewModel.text.value = Calculation.calDivided(this.sb)
+                this.viewModel.text.value = Calculation.calDivided(this.viewModel.sb.value!!)
             }
         }
         // 小数点以下が０の場合は削除する
--- app/src/main/java/com/example/dentaku_kotlin/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/dentaku_kotlin/ui/main/MainViewModel.kt
@@ -9,15 +9,21 @@ import java.lang.StringBuilder
 
 class MainViewModel(application: Application) : AndroidViewModel(application)  {
     var text : MutableLiveData<String> = MutableLiveData()
+    var sb: MutableLiveData<StringBuilder> = MutableLiveData()
     var opeFlg : MutableLiveData<OperatorFlg> = MutableLiveData()
     var equalFlg : MutableLiveData<Boolean> = MutableLiveData()
     var dotFlg : MutableLiveData<Boolean> = MutableLiveData()
     var opeEnableFlg : MutableLiveData<Boolean> = MutableLiveData()
     init {
+        this.sb.value = StringBuilder()
         this.opeFlg.value = OperatorFlg.DEFAULT
         this.equalFlg.value = false
         this.dotFlg.value = true
         this.opeEnableFlg.value = false
+    }
+
+    fun sbToText() {
+        this.text.value = this.sb.value.toString()
     }
 
 }
