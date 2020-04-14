9bb43efed621b0cbca62cfd5d7034bc0db3c96c2
ha-akimoto <nagiha0000@yahoo.co.jp>
2020-04-14 15:19:21 +0900

    修正：fragmentのボタン押下処理をViewModelに移動

--- app/src/main/java/com/example/dentaku_kotlin/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/dentaku_kotlin/ui/main/MainFragment.kt
@@ -8,8 +8,6 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.dentaku_kotlin.R
-import com.example.dentaku_kotlin.common.Calculation
-import com.example.dentaku_kotlin.common.Constants
 import com.example.dentaku_kotlin.common.OperatorFlg
 import com.example.dentaku_kotlin.databinding.MainFragmentBinding
 
@@ -21,7 +19,6 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
     private lateinit var binding: MainFragmentBinding
-    //private val sb: StringBuilder = java.lang.StringBuilder()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,148 +35,7 @@ class MainFragment : Fragment() {
         this.binding.defaultFlg = OperatorFlg.DEFAULT
         this.binding.lifecycleOwner = this
 
-        // リスナー設定
-        this.binding.button0.setOnClickListener {
-            this.viewModel.sb.value!!.append("0")
-            this.viewModel.sbToText()
-            flgSet()
-        }
-        this.binding.button1.setOnClickListener {
-            this.viewModel.sb.value!!.append("1")
-            this.viewModel.sbToText()
-            flgSet()
-        }
-        this.binding.button2.setOnClickListener {
-            this.viewModel.sb.value!!.append("2")
-            this.viewModel.sbToText()
-            flgSet()
-        }
-        this.binding.button3.setOnClickListener {
-            this.viewModel.sb.value!!.append("3")
-            this.viewModel.sbToText()
-            flgSet()
-        }
-        this.binding.button4.setOnClickListener {
-            this.viewModel.sb.value!!.append("4")
-            this.viewModel.sbToText()
-            flgSet()
-        }
-        this.binding.button5.setOnClickListener {
-            this.viewModel.sb.value!!.append("5")
-            this.viewModel.sbToText()
-            flgSet()
-        }
-        this.binding.button6.setOnClickListener {
-            this.viewModel.sb.value!!.append("6")
-            this.viewModel.sbToText()
-            flgSet()
-        }
-        this.binding.button7.setOnClickListener {
-            this.viewModel.sb.value!!.append("7")
-            this.viewModel.sbToText()
-            flgSet()
-        }
-        this.binding.button8.setOnClickListener {
-            this.viewModel.sb.value!!.append("8")
-            this.viewModel.sbToText()
-            flgSet()
-        }
-        this.binding.button9.setOnClickListener {
-            this.viewModel.sb.value!!.append("9")
-            this.viewModel.sbToText()
-            flgSet()
-        }
-        this.binding.buttonPlus.setOnClickListener {
-            this.viewModel.sb.value!!.append(Constants.PLUS)
-            this.viewModel.opeFlg.value = OperatorFlg.PLUS
-            this.viewModel.sbToText()
-            this.viewModel.dotFlg.value = true
-            this.viewModel.opeEnableFlg.value = false
-        }
-        this.binding.buttonMinus.setOnClickListener {
-            this.viewModel.sb.value!!.append(Constants.MINUS)
-            this.viewModel.opeFlg.value = OperatorFlg.MINUS
-            this.viewModel.sbToText()
-            this.viewModel.dotFlg.value = true
-            this.viewModel.opeEnableFlg.value = false
-        }
-        this.binding.buttonTimes.setOnClickListener {
-            this.viewModel.sb.value!!.append(Constants.TIMES)
-            this.viewModel.opeFlg.value = OperatorFlg.TIMES
-            this.viewModel.sbToText()
-            this.viewModel.dotFlg.value = true
-            this.viewModel.opeEnableFlg.value = false
-        }
-        this.binding.buttonDivided.setOnClickListener {
-            this.viewModel.sb.value!!.append(Constants.DIVIDED)
-            this.viewModel.opeFlg.value = OperatorFlg.DIVIDED
-            this.viewModel.sbToText()
-            this.viewModel.dotFlg.value = true
-            this.viewModel.opeEnableFlg.value = false
-        }
-        this.binding.buttonDot.setOnClickListener {
-            this.viewModel.sb.value!!.append(".")
-            this.viewModel.sbToText()
-            this.viewModel.dotFlg.value = false
-        }
-        this.binding.buttonEquals.setOnClickListener {
-            executeCalculation()
-            this.viewModel.opeFlg.value = OperatorFlg.DEFAULT
-            this.viewModel.sb.value!!.clear()
-            this.viewModel.dotFlg.value = true
-            this.viewModel.equalFlg.value = false
-            this.viewModel.opeEnableFlg.value = false
-        }
-        this.binding.buttonClear.setOnClickListener {
-            this.viewModel.sb.value!!.clear()
-            this.viewModel.text.value = null
-            this.viewModel.opeFlg.value = OperatorFlg.DEFAULT
-            this.viewModel.dotFlg.value = true
-            this.viewModel.equalFlg.value = false
-            this.viewModel.opeEnableFlg.value = false
-        }
-
         return this.binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        // ViewModelのテキストがNullか空文字じゃない場合
-        /*
-        if (!this.viewModel.text.value.isNullOrEmpty()) {
-            this.viewModel.sb.value!!.append(this.viewModel.text.value)
-        }
-
-         */
-    }
-
-    private fun executeCalculation() {
-        // それぞれの計算を呼び出す
-        when (this.viewModel.opeFlg.value) {
-            OperatorFlg.PLUS -> {
-                this.viewModel.text.value = Calculation.calPlus(this.viewModel.sb.value!!)
-            }
-            OperatorFlg.MINUS -> {
-                this.viewModel.text.value = Calculation.calMinus(this.viewModel.sb.value!!)
-            }
-            OperatorFlg.TIMES -> {
-                this.viewModel.text.value = Calculation.calTimes(this.viewModel.sb.value!!)
-            }
-            OperatorFlg.DIVIDED -> {
-                this.viewModel.text.value = Calculation.calDivided(this.viewModel.sb.value!!)
-            }
-        }
-        // 小数点以下が０の場合は削除する
-        this.viewModel.text.value = Calculation.zeroCut(this.viewModel.text.value!!)
-    }
-
-    private fun flgSet() {
-        // オペレータフラグがデフォルトの場合
-        if (this.viewModel.opeFlg.value == OperatorFlg.DEFAULT) {
-            this.viewModel.opeEnableFlg.value = true
-        } else {
-            this.viewModel.equalFlg.value = true
-        }
     }
 
 }
--- app/src/main/java/com/example/dentaku_kotlin/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/dentaku_kotlin/ui/main/MainViewModel.kt
@@ -1,29 +1,111 @@
 package com.example.dentaku_kotlin.ui.main
 
 import android.app.Application
+import android.view.View
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import com.example.dentaku_kotlin.R
+import com.example.dentaku_kotlin.common.Calculation
+import com.example.dentaku_kotlin.common.Constants
 import com.example.dentaku_kotlin.common.OperatorFlg
-import java.lang.StringBuilder
 
-class MainViewModel(application: Application) : AndroidViewModel(application)  {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
     var text : MutableLiveData<String> = MutableLiveData()
-    var sb: MutableLiveData<StringBuilder> = MutableLiveData()
+    var sb: StringBuilder = StringBuilder()
     var opeFlg : MutableLiveData<OperatorFlg> = MutableLiveData()
     var equalFlg : MutableLiveData<Boolean> = MutableLiveData()
     var dotFlg : MutableLiveData<Boolean> = MutableLiveData()
     var opeEnableFlg : MutableLiveData<Boolean> = MutableLiveData()
+
     init {
-        this.sb.value = StringBuilder()
         this.opeFlg.value = OperatorFlg.DEFAULT
         this.equalFlg.value = false
         this.dotFlg.value = true
         this.opeEnableFlg.value = false
     }
 
-    fun sbToText() {
-        this.text.value = this.sb.value.toString()
+    fun onClickNumber(number: Char) {
+        this.sb.append(number)
+        this.text.value = this.sb.toString()
+        flgSet()
+    }
+
+    fun onClickOperator(view: View) {
+        when (view.id) {
+            R.id.button_plus -> {
+                this.sb.append(Constants.PLUS)
+                this.opeFlg.value = OperatorFlg.PLUS
+            }
+            R.id.button_minus -> {
+                this.sb.append(Constants.MINUS)
+                this.opeFlg.value = OperatorFlg.MINUS
+            }
+            R.id.button_times -> {
+                this.sb.append(Constants.TIMES)
+                this.opeFlg.value = OperatorFlg.TIMES
+            }
+            R.id.button_divided -> {
+                this.sb.append(Constants.DIVIDED)
+                this.opeFlg.value = OperatorFlg.DIVIDED
+            }
+        }
+        this.text.value = this.sb.toString()
+        this.dotFlg.value = true
+        this.opeEnableFlg.value = false
+    }
+
+    fun onClickDot() {
+        this.sb.append(".")
+        this.text.value = this.sb.toString()
+        this.dotFlg.value = false
+    }
+
+    fun onClickClear() {
+        this.sb.clear()
+        this.text.value = null
+        this.opeFlg.value = OperatorFlg.DEFAULT
+        this.dotFlg.value = true
+        this.equalFlg.value = false
+        this.opeEnableFlg.value = false
+    }
+
+    fun onClickEqual() {
+        executeCalculation()
+        this.opeFlg.value = OperatorFlg.DEFAULT
+        this.sb.clear()
+        this.dotFlg.value = true
+        this.equalFlg.value = false
+        this.opeEnableFlg.value = false
+    }
+
+    private fun flgSet() {
+        // オペレータフラグがデフォルトの場合
+        if (this.opeFlg.value == OperatorFlg.DEFAULT) {
+            this.opeEnableFlg.value = true
+        } else {
+            this.equalFlg.value = true
+        }
+    }
+
+    private fun executeCalculation() {
+
+        when (this.opeFlg.value) {
+            OperatorFlg.PLUS -> {
+                this.text.value = Calculation.calPlus(this.sb)
+            }
+            OperatorFlg.MINUS -> {
+                this.text.value = Calculation.calMinus(this.sb)
+            }
+            OperatorFlg.TIMES -> {
+                this.text.value = Calculation.calTimes(this.sb)
+            }
+            OperatorFlg.DIVIDED -> {
+                this.text.value = Calculation.calDivided(this.sb)
+            }
+        }
+        // 小数点以下が０の場合は削除する
+        this.text.value = Calculation.zeroCut(this.text.value!!)
+
     }
 
 }
